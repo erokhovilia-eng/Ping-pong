664b535426774cc9460adc9ee045fe5d8c831a0c
erokhovilia-eng <erokhov.ilia@kischool.ru>
2026-04-19 13:31:26 +0300

    Update main.py
    Добавление классов и оформление заднего плана

--- main/main/main.py
+++ main/main/main.py
@@ -1,1 +1,35 @@
+from pygame import *
 
+win_width = 600
+win_height = 500
+win = display.set_mode((win_width, win_height))
+display.set_caption('Пинг-понг')
+win.fill((200, 255, 255))
+
+font.init()
+clock = time.Clock()
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, x, y, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = speed
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def reset(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
